53e545f1a9ca8c8362702a36f5c5d28f23ba2bf6
JuanCubero <juanelias.cuberom@gmail.com>
2016-10-05 17:08:37 -0600

    Create migrations tables(update_user, Student, Loans, MoneyPenalty)

--- database/migrations/2016_10_04_180447_create_students_table.php
+++ database/migrations/2016_10_04_180447_create_students_table.php
@@ -14,6 +14,9 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('license');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
--- database/migrations/2016_10_04_180838_create_loans_table.php
+++ database/migrations/2016_10_04_180838_create_loans_table.php
@@ -14,6 +14,14 @@ class CreateLoansTable extends Migration
     {
         Schema::create('loans', function (Blueprint $table) {
             $table->increments('id');
+            $table->datetime('departure_time');
+            $table->datetime('return_time');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_ud')->references('id')->on('user');
+            $table->integer('authorizing_user_id')->unsigned();
+            $table->foreign('authorizing_user_id')->references('id')->on('user');
+            $table->integer('lonable_id')->unsigned();
+            $table->foreign('lonable_id')->references('id')->on('lonable');
             $table->timestamps();
         });
     }
